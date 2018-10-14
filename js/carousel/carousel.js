d3937649b628cdbb365f1e828735b14b78bcd5ca
Yuliya Yachmenyuk <yachmenyuk@gmail.com>
2018-10-15 00:37:37 +0300

    bug fixed

--- js/carousel/carousel.js
+++ js/carousel/carousel.js
@@ -1,54 +1,54 @@
 /*Carousel*/
-	$(function() {
-		var thumbs = $('#thumbscarousel');
-	 
-		$('#carousel').carouFredSel({
-			items: 1,
-			scroll: {
-				fx: 'crossfade'
-			},
-			auto: {
-				timeoutDuration: 5000,
-				duration: 2000
-			},
-			pagination: {
-				container: '#pager',
-				duration: 1500
-			}
-		});
-	 
-		thumbs.carouFredSel({
-			circular: false,
-			auto: false,
-			width: 150,
-			height: 75,
-			scroll: {
-				duration: 200
-			},
-			items: {
-				visible: 1,
-				width: 150,
-				height: 75
-			}
-		});
-	 
-		$('#pager').hover(function() {
-			var current = $('#carousel').triggerHandler( 'currentPosition' );
-			thumbs.trigger( 'slideTo', [ current, 0, true, { fx: 'none' } ] );
-			$('#thumbs').stop().fadeTo(300, 1);
-		}, function() {
-			$('#thumbs').stop().fadeTo(300, 0);
-		});
-	 
-		$('#pager a').mouseenter(function() {
-			var index = $('#pager a').index( $(this) );
-	 
-			//	clear the queue
-			thumbs.trigger( 'queue', [[]] );
-	 
-			//	scroll
-			thumbs.trigger( 'slideTo', [index, { queue: true }] );
-		});
+$(document).ready(function() {
+	var thumbs = $('#thumbscarousel');
+ 
+	$('#carousel').carouFredSel({
+		items: 1,
+		scroll: {
+			fx: 'crossfade'
+		},
+		auto: {
+			timeoutDuration: 5000,
+			duration: 2000
+		},
+		pagination: {
+			container: '#pager',
+			duration: 1500
+		}
 	});
+ 
+	thumbs.carouFredSel({
+		circular: false,
+		auto: false,
+		width: 150,
+		height: 75,
+		scroll: {
+			duration: 200
+		},
+		items: {
+			visible: 1,
+			width: 150,
+			height: 75
+		}
+	});
+ 
+	$('#pager').hover(function() {
+		var current = $('#carousel').triggerHandler( 'currentPosition' );
+		thumbs.trigger( 'slideTo', [ current, 0, true, { fx: 'none' } ] );
+		$('#thumbs').stop().fadeTo(300, 1);
+	}, function() {
+		$('#thumbs').stop().fadeTo(300, 0);
+	});
+ 
+	$('#pager a').mouseenter(function() {
+		var index = $('#pager a').index( $(this) );
+ 
+		//	clear the queue
+		thumbs.trigger( 'queue', [[]] );
+ 
+		//	scroll
+		thumbs.trigger( 'slideTo', [index, { queue: true }] );
+	});
+});
 
 
